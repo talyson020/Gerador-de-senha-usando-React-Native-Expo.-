af644afefc72e8ecd63253f54ec03f1f6ce0bc7e
talysonmoraes2@gmail.com <talysonmoraes2@gmail.com>
2024-03-01 23:15:06 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native'
+import{Routes} from './SRC/routes'
+
+
+
+export default function App() {
+ return (
+    <NavigationContainer>
+     <Routes/>
+    </NavigationContainer>
+  )
+}
